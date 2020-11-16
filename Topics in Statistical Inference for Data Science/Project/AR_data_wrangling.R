aa0dad3844746bcacd06725e5ede2f54cccb5b04
Joshua <joshingram2018@gmail.com>
2020-11-16 09:22:59 -0500

    finished chapter 16 summaries

--- Topics in Statistical Inference for Data Science/Project/AR_data_wrangling.R
+++ Topics in Statistical Inference for Data Science/Project/AR_data_wrangling.R
@@ -1,5 +1,4 @@
 # SI Solar Flare Project Data Wrangling
-
 library(tidyverse)
 library(VGAM)
 library(tm)
@@ -7,6 +6,7 @@ library(hash)
 library(lubridate)
 library(fpp2)
 library(zoo)
+library(reshape)
 
 RHESSI <- read_csv("D:/main/Datasets/Solar_REU/RHESSI_data/RHESSI_clean.csv")
 RHESSI$cycle <- as.factor(RHESSI$cycle)
@@ -82,13 +82,34 @@ length <- difftime(end_time, start_time)
 region_1_ts <- ts(region_1_TE$avg_TE, )
 autoplot(region_1_ts)
 
-# for loop that goes through equidistant time intervals until time runs out... make this the same for each region, take mean in each
+date <- as.POSIXlt("2004-01-01 00:40:06")
+mean_TE <- c()
+time <- C()
 
+while (date <= as.POSIXlt("2004-01-15 08:18:38")){
+  date_1 <- date + minutes(150)
+  
+  mean_current <- mean(region_1[which(region_1$Rpeak < date_1 & region_1$Rpeak >= date),]$RFlrTotalEnergy)
+  
+  mean_TE <- c(mean_TE, mean_current)
+  time <- c(time, date_1)
+  
+  date <- date_1
+}
 
+mean_TE["=-Inf"] <- 0
+ts_region1 <- ts(log(mean_TE))
+autoplot(ts_region1)
+ts_region1 <- as.data.frame(ts_region1)$x
+ts_region1[is.nan(ts_region1)] <- 0
+
+write.csv(ts_region1, file = "D:/main/Projects/undergraduate-coursework/Topics in Statistical Inference for Data Science/Project/data/region1.csv")
+
+plot()
 
 # region 2 - Occurred in Aug. 2004
 # start - "2004-08-05 05:26:50 UTC"
-# end - "2004-08-20 02:30:46 UTC"
+# end - "2004-08-18 04:30:46 UTC"
 # length - 14.8777314814815
 region_2 <- RHESSI[which(RHESSI$RARreg == regions[2]),]
 
@@ -103,7 +124,28 @@ length <- difftime(end_time, start_time)
 region_2_ts <- ts(region_2_TE$avg_TE)
 autoplot(region_2_ts)
 
+date <- as.POSIXlt("2004-08-05 05:26:50 UTC")
+mean_TE <- c()
+time <- c()
 
+while (date <= as.POSIXlt("2004-08-18 04:30:46 UTC")){
+  date_1 <- date + minutes(150)
+  
+  mean_current <- mean(region_2[which(region_2$Rpeak < date_1 & region_2$Rpeak >= date),]$RFlrTotalEnergy)
+  
+  mean_TE <- c(mean_TE, mean_current)
+  time <- c(time, date_1)
+  
+  date <- date_1
+}
+
+mean_TE["=-Inf"] <- 0
+ts_region2 <- ts(log(mean_TE))
+autoplot(ts_region2)
+ts_region2 <- as.data.frame(ts_region2)$x
+ts_region2[is.nan(ts_region2)] <- 0
+
+write.csv(ts_region2, file = "D:/main/Projects/undergraduate-coursework/Topics in Statistical Inference for Data Science/Project/data/region2.csv")
 
 # region 3 - Occurred in Jan. 2005
 # start - "2005-01-10 15:01:17 UTC"
@@ -122,7 +164,28 @@ length <- difftime(end_time, start_time)
 region_3_ts <- ts(region_3_TE$avg_TE)
 autoplot(region_3_ts)
 
+date <- as.POSIXlt("2005-01-10 15:01:17 UTC")
+mean_TE <- c()
+time <- c()
 
+while (date <= as.POSIXlt("2005-01-23 20:54:53 UTC")){
+  date_1 <- date + minutes(150)
+  
+  mean_current <- mean(region_3[which(region_3$Rpeak < date_1 & region_3$Rpeak >= date),]$RFlrTotalEnergy)
+  
+  mean_TE <- c(mean_TE, mean_current)
+  time <- c(time, date_1)
+  
+  date <- date_1
+}
+
+mean_TE["=-Inf"] <- 0
+ts_region3 <- ts(log(mean_TE))
+autoplot(ts_region3)
+ts_region3 <- as.data.frame(ts_region3)$x
+ts_region3[is.nan(ts_region3)] <- 0
+
+write.csv(ts_region3, file = "D:/main/Projects/undergraduate-coursework/Topics in Statistical Inference for Data Science/Project/data/region3.csv")
 
 # region 4 - occurred in Sept. 2005
 # start - "2005-09-06 13:48:41 UTC"
@@ -141,7 +204,28 @@ length <- difftime(end_time, start_time)
 region_4_ts <- ts(region_4_TE$avg_TE)
 autoplot(region_4_ts)
 
+date <- as.POSIXlt("2005-09-06 13:48:41 UTC")
+mean_TE <- c()
+time <- c()
 
+while (date <= as.POSIXlt("2005-09-21 04:09:25 UTC")){
+  date_1 <- date + minutes(150)
+  
+  mean_current <- mean(region_4[which(region_4$Rpeak < date_1 & region_4$Rpeak >= date),]$RFlrTotalEnergy)
+  
+  mean_TE <- c(mean_TE, mean_current)
+  time <- c(time, date_1)
+  
+  date <- date_1
+}
+
+mean_TE["=-Inf"] <- 0
+ts_region4 <- ts(log(mean_TE))
+autoplot(ts_region4)
+ts_region4 <- as.data.frame(ts_region4)$x
+ts_region4[is.nan(ts_region4)] <- 0
+
+write.csv(ts_region4, file = "D:/main/Projects/undergraduate-coursework/Topics in Statistical Inference for Data Science/Project/data/region4.csv")
 
 # region 5 - Occurred in Oct. 2014
 # start - "2014-10-16 07:44:33 UTC"
@@ -159,3 +243,26 @@ length <- difftime(end_time, start_time)
 
 region_5_ts <- ts(region_5_TE$avg_TE)
 autoplot(region_5_ts)
+
+date <- as.POSIXlt("2014-10-16 07:44:33 UTC")
+mean_TE <- c()
+time <- c()
+
+while (date <= as.POSIXlt("2014-11-01 10:04:13 UTC")){
+  date_1 <- date + minutes(150)
+  
+  mean_current <- mean(region_5[which(region_5$Rpeak < date_1 & region_5$Rpeak >= date),]$RFlrTotalEnergy)
+  
+  mean_TE <- c(mean_TE, mean_current)
+  time <- c(time, date_1)
+  
+  date <- date_1
+}
+
+mean_TE["=-Inf"] <- 0
+ts_region5 <- ts(log(mean_TE))
+autoplot(ts_region5)
+ts_region5 <- as.data.frame(ts_region5)$x
+ts_region5[is.nan(ts_region5)] <- 0
+
+write.csv(ts_region5, file = "D:/main/Projects/undergraduate-coursework/Topics in Statistical Inference for Data Science/Project/data/region5.csv")
